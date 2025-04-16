d133867c1b5e85e4ad74573615a8dfb109a27766
jayce <15202844993@163.com>
2025-04-16 17:04:17 +0800

    commit

---
+++ src/basic/syntax.kt
@@ -0,0 +1,14 @@
+package basic
+
+//fun main() {
+//    println("hello world")
+//}
+
+//fun main(args: Array<String>) {
+//    println(args.contentToString())
+//}
+
+fun main(){
+    //sample start
+    print("Hello")
+}

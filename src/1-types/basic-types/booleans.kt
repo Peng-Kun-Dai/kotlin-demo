cf8ab1d2d0bfcfa3c96c9808473eea623f0b52d2
jayce <15202844993@163.com>
2025-04-18 14:11:50 +0800

    commit

---
+++ src/1-types/basic-types/booleans.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var myTrue: Boolean = true
+    var myFalse: Boolean = false
+    var boolNull: Boolean? = null
+
+    println(myTrue || myFalse)
+    println(myTrue && myFalse)
+    println(!myTrue)
+    println(boolNull)
+}
